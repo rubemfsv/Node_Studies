dda5e7f6c6b1a8e15373707e41d8712c4966bdfc
Rubem <rfsv@ic.ufal.br>
2020-07-27 18:47:51 -0300

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1037444553374868',
+        clientSecret: '6b801d87440b3cc71611d7092fef4036'
+    }
 }
